2552eccf9a269a47e155b286266d0a5dafdaaed2
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2020-01-04 16:07:29 +0200

    DStructure 1.3.1 Добавлена поддержка нового Qero

--- DStructure.php
+++ DStructure.php
@@ -1,6 +1,27 @@
 <?php
 
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * @package     Distributed Structure
+ * @copyright   2019 - 2020 Podvirnyy Nikita (Observer KRypt0n_)
+ * @license     GNU GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>
+ * @author      Podvirnyy Nikita (Observer KRypt0n_)
+ * 
+ * Contacts:
+ *
+ * Email: <suimin.tu.mu.ga.mi@gmail.com>
+ * VK:    vk.com/technomindlp
+ *        vk.com/hphp_convertation
+ * 
+ */
+
 namespace DStructure;
+
+const DS = DIRECTORY_SEPARATOR;
 
 require 'php/Item.php';
 require 'php/Structure.php';
